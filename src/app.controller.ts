037dacaa8df7a676cfbe8f90ca0bc996560b087e
nawiwat <phuphapupa84@gmail.com>
2022-11-23 04:20:44 +0700

    a

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,20 +15,27 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/hotelnearme')
-  getNearHotel() {
-    return this.appService.getNearHotel();
+  @Get('/:input')
+  getNearHotel(@Param('input') input: string) {
+    if (input === 'hotelnearme') {
+      return this.appService.getNearHotel();
+    }
+    if (input === 'cheaphotel') {
+      return this.appService.getCheapHotel();
+    } else {
+      return this.appService.searhHotel(input);
+    }
   }
 
-  @Get('/search/:value')
-  searchHotel(@Param('value') value: string) {
-    return this.appService.searhHotel(value);
-  }
+  // @Get('/search/:value')
+  // searchHotel(@Param('value') value: string) {
+  //   return this.appService.searhHotel(value);
+  // }
 
-  @Get('/cheaphotel')
-  getCheapHotel() {
-    return this.appService.getCheapHotel();
-  }
+  // @Get('/cheaphotel')
+  // getCheapHotel() {
+  //   return this.appService.getCheapHotel();
+  // }
 
   // @Post()
   // create(@Body() createUserDto: CreateUserDto) {
